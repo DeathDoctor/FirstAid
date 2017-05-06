02353593172aa7b5fb474f10e972477148550517
DeathDoctor <deathdoctor4499@gmail.com>
2017-05-06 18:33:49 +0200

    gestes de secours créée

---
+++ app/src/main/java/com/example/lkhedma/firstaid/GestesDeSecoursBrulure.java
@@ -0,0 +1,13 @@
+package com.example.lkhedma.firstaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GestesDeSecoursBrulure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestes_de_secours_brulure);
+    }
+}
